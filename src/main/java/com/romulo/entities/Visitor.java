30c0660350300bec5d6c0d9a8bd4d0ec6492359d
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-10-16 15:20:57 -0300

    ORM mapping with JPA in visitor class

--- src/main/java/com/romulo/entities/Visitor.java
+++ src/main/java/com/romulo/entities/Visitor.java
@@ -3,16 +3,23 @@ package com.romulo.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "tb_visitor")
 public class Visitor implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@Column(name = "visitor_id")
 	private Long id;
 	private String name;
 	
-	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant instantRegistration;
 	
 	public Visitor() {
